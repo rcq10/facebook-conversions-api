67d2b65613d1829b1c6f51941f5709387b3ec2e0
rcq10 <ramoncanjirana@gmail.com>
2025-08-02 01:00:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,56 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const cors = require('cors'); // Adicionado o pacote CORS
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors()); // Ativa o CORS para todas as requisições
+app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/purchase', async (req, res) => { // Endpoint ajustado para '/purchase'
-  const {
-    event_name,
-    event_time,
-    event_source_url,
-    fbp,
-    fbclid,
-    user_agent,
-    ip_address,
-    purchase_value
-  } = req.body;
+app.post('/purchase', async (req, res) => {
+  const { user_data, custom_data, event_name, event_time, action_source, event_id, event_source_url } = req.body;
 
-  // Adicionado o 'fbclid' no 'user_data' e o 'client_user_agent'
   const payload = {
     data: [
       {
-        event_name: event_name,
-        event_time: event_time,
+        event_name: event_name || 'Purchase',
+        event_time: event_time || Math.floor(Date.now() / 1000),
         event_source_url: event_source_url,
         user_data: {
-          client_user_agent: user_agent,
-          fbp: fbp,
-          fbclid: fbclid, // Corrigido para 'fbclid' em vez de 'fbc'
-          client_ip_address: ip_address,
+          client_user_agent: user_data?.client_user_agent,
+          fbp: user_data?.fbp,
+          fbclid: user_data?.fbclid,
+          client_ip_address: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
         },
         custom_data: {
-          currency: "BRL",
-          value: purchase_value
+          currency: custom_data?.currency || 'BRL',
+          value: custom_data?.value || 1.00,
         },
-        action_source: "website"
+        action_source: action_source || 'website',
+        event_id: event_id,
       }
-    ]
+    ],
+    test_event_code: null, // Deixe como null em produção
   };
+
+  const PIXEL_ID = '1124756639573111'; // Seu Pixel ID
+  const ACCESS_TOKEN = 'EAAKSZCfKLHRkBPAWRqy9KF4S8nCr2UUvdKyBw0PfKtn2dI5Q35BehgXfrxERkdPJnRBBV68EBrEiRZAAbZBxOz2ipC7CFGRpmxfOzZA03OFi3ZCtxlj9RErceV6u7gyFgjG9AcVOHPgsLsLrrphJOyl3yaVmwqvwPkvxtPHodZCvTAthZClsrAaXq6EMlEupTtGtAZDZD'; // Seu Token
 
   try {
     const response = await axios.post(
-      'https://graph.facebook.com/v19.0/1124756639573111/events?access_token=EAAKSZCfKLHRkBPNrZB4j2loUnxDSj3Pwii1yc0PHnQUZB7JX0i0eL557MP75Y5WIpwgI2m5OcfTvXNbCvPd4875Id3ZBmu38xdAOlTKdMWCyWYzGZAwq0lyMWUSPCojKxnRgLZBoLDgJuh7Cvcg4Ku514MYIK2CZBTusaBJfn7Rxh8uRNNXtdxnZCPWmxxLJTsjkqAZDZD',
+      `https://graph.facebook.com/v19.0/${PIXEL_ID}/events?access_token=${ACCESS_TOKEN}`,
       payload
     );
     res.status(200).send({ success: true, response: response.data });
   } catch (error) {
     console.error(error.response ? error.response.data : error.message);
-    res.status(500).send({ success: false, error: error.message });
+    res.status(500).send({ success: false, error: error.response?.data });
   }
 });
 
